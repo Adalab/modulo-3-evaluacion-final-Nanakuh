3cc69c5404c69cda4ef0aebc5318993fe2bcee58
Ana Gil <anagilrodrig@gmail.com>
2024-02-14 07:25:36 +0100

    Footer created

---
+++ src/components/pages/footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+
+   return (
+    <footer className="footer">
+    <div className="footer-content">
+      <p>
+        &copy; {new Date().getFullYear()} Fun with Harry Potter. Todos los derechos
+        reservados.
+      </p>
+    </div>
+    </footer>
+   );
+  }
+
+export default Footer;
