3e324d0e8102430acfa61526bee83fd75e63c719
Ana Gil <anagilrodrig@gmail.com>
2024-02-26 07:34:24 +0100

    Fixing text in name-filter

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Detail from "./pages/detail/detail";
 import Home from "./pages/home/home";
+import { useState } from "react";
 
 function App() {
+  const [textFilter] = useState("");
   
   return (
     <Routes>
-      <Route path="/" element={<Home/>} /> 
+      <Route path="/" element={<Home/>} textFilter={textFilter}/> 
       <Route path="/character/:id" element={<Detail/>} />
     </Routes>
   );
